d62535bff5e18bc61eca083e6760e5a873b2cd31
David Kiener <kiener.33@buckeyemail.osu.edu>
2025-08-09 18:40:26 -0400

    initial commit

---
+++ ohio_unemployment.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+path = Path('OHUR.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+for index, col_title in enumerate(header_row):
+    print(f"{index} {col_title}, ", end=' ')
+print()
+
+dates = []
+percentages = []
+
+for row in reader:
+    try:
+        current_date = datetime.strptime(row[0], '%Y-%m-%d')
+        percent = float(row[1])
+    except ValueError as e:
+        print(current_date)
+    else:
+        dates.append(current_date)
+        percentages.append(percent)
+
+# plt.style.use('dark_background')
+figure, graph = plt.subplots()
+
+graph.plot(dates, percentages, color='blue')
+# graph.fill_between(dates, high_temps, low_temps, color='blue', alpha=0.5)
+
+graph.set_title('Ohio Unemployment (by Month): 1976 - 2022', fontsize='24')
+graph.set_xlabel('Date', fontsize=16)
+graph.set_ylabel('Unemp Rate', fontsize=16)
+figure.autofmt_xdate()
+
+plt.show()
---
+++ sitka_temps.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+path = Path('sitka_weather_2018_full.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+for index, col_title in enumerate(header_row):
+    print(f"{index} {col_title}, ", end=' ')
+print()
+
+dates = []
+high_temps = []
+low_temps = []
+
+for row in reader:
+    try:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[8])
+        low = int(row[9])
+    except ValueError as e:
+        print(current_date)
+    else:
+        dates.append(current_date)
+        high_temps.append(high)
+        low_temps.append(low)
+
+plt.style.use('dark_background')
+figure, graph = plt.subplots()
+
+graph.plot(dates, high_temps, color='red')
+graph.plot(dates, low_temps, color='blue')
+graph.fill_between(dates, high_temps, low_temps, color='blue', alpha=0.5)
+
+graph.set_title('Daily Temps for 2018 in Sitka, Alaska', fontsize='24')
+graph.set_ylabel('Temp (F)', fontsize=16)
+figure.autofmt_xdate()
+
+plt.show()
